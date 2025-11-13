08995e65a88f36c7f2d6a64666b25d2898816025
smahan blm <boulmaneismahane@gmail.com>
2025-11-13 22:38:54 +0100

    configuration scene builder

--- src/main/java/com/stock/Main.java
+++ src/main/java/com/stock/Main.java
@@ -2,19 +2,25 @@ package com.stock;
 
 
 import javafx.application.Application;
-import javafx.scene.Parent;
-
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Magasinier/DashboardMagasinier.fxml"));
+
+        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/Magasinier/DashboardMagasinier.fxml"));
 
 
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Dashboard_chef_vets/DashboardChef_vents.fxml"));
+        //charger le contenu du fichier dashboard.fxml
+        Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Stock Manager");
